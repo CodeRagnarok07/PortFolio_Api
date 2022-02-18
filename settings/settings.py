6919381cfde4f88bbce3f68c50198c112b70b0a6
Angel <angelriera1796@gmail.com>
2022-02-18 11:12:13 -0400

    navbar no work

--- settings/settings.py
+++ settings/settings.py
@@ -116,10 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 
 
 # Default primary key field type
--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,6 @@
-navbotton = document.querySelector("#navboton")
-navmenu = document.querySelector("#navmenu")
+const navbotton = document.querySelector("#navboton")
+const navmenu = document.querySelector("#navmenu")
 
-navbotton.addEventListener("click", () => {
-    navmenu.classList.toggle("hidden")
+navbotton.addEventListener('click', () => { // al hacer click en el elemento despliege
+  navmenu.classList.toggle("hidden") // le agrega la clase hidden al objeto itens
 })
