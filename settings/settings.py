58a6eb7257bdc55a074bd599927289e5b9663ba3
rierangel <angelriera1796@gmail.com>
2022-08-12 09:32:22 -0400

    allow host

--- settings/settings.py
+++ settings/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOST = env('ALLOWED_HOST')
+ALLOWED_HOST = [env('ALLOWED_HOSTS')]
 
 
 # Application definition
